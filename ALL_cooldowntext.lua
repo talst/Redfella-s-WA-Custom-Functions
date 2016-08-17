77a226f32f69931be97ba0485e2ceffd7857d21e
Reijo Nyberg <reijo.nyberg+github@gmail.com>
2016-08-17 23:15:28 +0300

    Improve Cooldowntext custom function
    Show charges/seconds for abilities that have charges
    Show "USED" for items that are used in combat and don't cooldown before
    combat is over

--- ALL_cooldowntext.lua
+++ ALL_cooldowntext.lua
@@ -1,24 +1,48 @@
 function()
     if aura_env.state then
-        local start, duration
+        local charges, maxCharges, start, duration, charges, rounded
+        local itemtype = 0
+        local charges = 0
+        local maxCharges = 0
+        local retstring = ""
 
-        if aura_env.state.trigger.event == 'Cooldown Progress (Spell)' then
-           start, duration = GetSpellCooldown(aura_env.state.trigger.spellName)
+        -- Cooldown Progress (Spell)
+        if not aura_env.state.trigger.itemName and aura_env.state.trigger.spellName then
+            charges, maxCharges, start, duration = GetSpellCharges(aura_env.state.trigger.spellName)
+
+            if charges == nil then
+                charges = 0
+                maxCharges = 0
+                start, duration = GetSpellCooldown(aura_env.state.trigger.spellName)
+            end
         end
 
-        if aura_env.state.trigger.event == 'Cooldown Progress (Item)' then
+        -- Cooldown Progress (Item)
+        if aura_env.state.trigger.itemName and aura_env.state.trigger.spellName then
             start, duration = GetItemCooldown(aura_env.state.trigger.itemName)
+            itemtype = select(8,GetItemInfo(109223))
         end
+
 
         if start and duration then
             local cooldown = start + duration - GetTime()
+
+            -- hack for items that stack to 20 and have 60 fixed CD during combat
+            if itemtype == 20 and cooldown == 60 then
+              return "USED"
+            end
+
             if cooldown > 0 then
                 if cooldown < 60 then
-                    return string.format("%.0f", cooldown) .. ""
-                else
-                    local rounded = math.floor(cooldown / 60 + 0.5)
-                    return string.format("%.0f", rounded) .. "m"
+                    retstring = string.format("%.0f", cooldown)
+                    if maxCharges > 0 then retstring = charges .. "/" .. retstring end
+                elseif cooldown >= 60 then
+                    rounded = math.floor(cooldown / 60 + 0.5)
+                    retstring = string.format("%.0f", rounded) .. "m"
+                    if maxCharges > 0 then retstring = charges .. "/" .. retstring end
                 end
+
+                return retstring
             end
         end
     end
